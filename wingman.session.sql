9882d178af9e4b9c09ed7c1b0e7537a7b2dd474a
Aneesh02 <aneeshtickoo2002@gmail.com>
2024-02-07 13:12:38 +0530

    Render compatibility

--- wingman.session.sql
+++ wingman.session.sql
@@ -1,1 +1,46 @@
-SELECT * FROM users;
+SELECT * FROM restaurants;
+
+DELETE FROM restaurants; 
+
+
+INSERT INTO restaurants (
+    name, 
+    street_address, 
+    category, 
+    price_range, 
+    owner_id, 
+    image_url, 
+    created_at, 
+    updated_at
+    )
+VALUES 
+  ('Tech Cafe', 
+  'Boys Hostel 2', 
+  'Indian', 
+  2, 
+  12, 
+  'https://vancouber-eats.s3.us-west-2.amazonaws.com/res1-main-image.jpg', 
+  CURRENT_DATE, 
+  CURRENT_DATE
+  );
+
+
+
+INSERT INTO menu_items (
+    restaurant_id, 
+    name, 
+    type, 
+    price, 
+    description, 
+    image_url, 
+    created_at, 
+    updated_at)
+VALUES 
+  (33, 
+  'Veg Maggi', 
+  'entree', 
+  50, 
+  'Quick noodles with vegetables', 
+  'https://vancouber-eats.s3.us-west-2.amazonaws.com/res1-main-image.jpg', 
+  CURRENT_DATE, 
+  CURRENT_DATE);
